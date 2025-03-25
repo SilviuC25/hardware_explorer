61a76de48f076220e9dcbe96cadfb9742ad1bb4c
SilviuC25 <167734907+SilviuC25@users.noreply.github.com>
2025-03-25 19:17:43 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header";
 import HeroBanner from "@/components/HeroBanner";
 import Footer from "@/components/Footer";
-import Image from "next/image";
 
 export default function Home() {
   return (
